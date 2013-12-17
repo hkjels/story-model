85cf8f51f70fff9cece63b65d80e04545ce4da7f
Henrik Kjelsberg <hkjels@me.com>
2013-12-17 14:19:15 +0100

    Scaffold for writing test

--- index.js
+++ index.js
@@ -13,15 +13,38 @@ var model = require('model')
 var Story = model('Story')
   .attr('id', { type: 'number' })
   .attr('title', { required: true, type: 'string' })
-  .attr('total', { type: 'number' })
-  .attr('closed', { type: 'number' });
+  .attr('timeTotal', { type: 'number' })
+  .attr('timeClosed', { type: 'number' });
+
+/**
+ * Tasks will be stored as a collection
+ * in each story.
+ */
 
 Story.prototype.tasks = new Collection();
 
-Story.prototype.open = function() {
-  return this.total() - this.closed();
+/**
+ * Time left until story is completed.
+ */
+
+Story.prototype.timeOpen = function() {
+  return this.timeTotal() - this.timeClosed();
 };
 
+/**
+ * Add task to collection.
+ */
+
+Story.prototype.addTask = function(task) {
+  if (!'Task' instanceof task) {
+    throw new Error('You must supply a valid Task');
+  }
+  this.timeTotal(this.timeTotal + task.estimate());
+  if ('done' == task.state()) {
+    this.timeClosed(this.timeClosed() + task.estimate());
+  }
+  this.tasks.push(task);
+};
 
 /**
  * Expose `Story`.
---
+++ test/story.js
@@ -0,0 +1,16 @@
+
+var Story = require('story-model');
+var assert = require('assert');
+
+describe('Story model', function () {
+
+  var story;
+
+  before(function() {
+  });
+
+  it ('should return a valid model-instance', function(done) {
+  });
+
+});
+
